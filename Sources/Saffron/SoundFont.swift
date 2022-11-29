9af6f6c7e101f0cd890b8e96927f0ab06490401a
Jere Käpyaho <2261936+jerekapyaho@users.noreply.github.com>
2022-11-29 11:26:55 +0200

    Endianness fix

--- Sources/Saffron/SoundFont.swift
+++ Sources/Saffron/SoundFont.swift
@@ -39,78 +39,27 @@ enum SoundFontError: Error {
     case samplePoolOverflow
 }
 
-// From https://stackoverflow.com/a/47221437/1016326
+
 extension FixedWidthInteger {
-    var byteWidth: Int {
-        return self.bitWidth / UInt8.bitWidth
-    }
-    static var byteWidth: Int {
-        return Self.bitWidth / UInt8.bitWidth
+    var bytes: ByteArray {
+        withUnsafeBytes(of: littleEndian, Array.init)
     }
 }
 
-// Adapted from https://stackoverflow.com/a/47513234/1016326
 extension String {
-    public init(fourCC: FourCC) {
-        let n = Int(fourCC)
-        var s = ""
-        
-        let unicodes = [
-            UnicodeScalar((n >> 24) & 255),
-            UnicodeScalar((n >> 16) & 255),
-            UnicodeScalar((n >> 8) & 255),
-            UnicodeScalar(n & 255)
-        ]
-        unicodes.compactMap { (unicode) -> String? in
-            guard let unicode = unicode else {
-                return nil
-            }
-            return String(unicode)
-        }.forEach { (unicode) in
-            s.append(unicode)
-        }
-        
-        self = s.trimmingCharacters(in: CharacterSet.whitespaces)
-    }
-    
     // Convert from string to FourCC (thanks, AudioKit!)
     public func toFourCC() -> FourCC {
-        let utf8 = self.utf8
-        precondition(utf8.count == 4, "Must be a four-character string")
+        guard self.utf8.count == 4 else {
+            return 0
+        }
+        
         var out: UInt32 = 0
-        for char in utf8 {
+        for char in self.utf8 {
             out <<= 8
             out |= UInt32(char)
         }
+        
         return out
-    }
-}
-
-func _convertToBytes<T>(_ value: T, withCapacity capacity: Int) -> [UInt8] {
-    var mutableValue = value
-    return withUnsafePointer(to: &mutableValue) {
-        return $0.withMemoryRebound(to: UInt8.self, capacity: capacity) {
-            return Array(UnsafeBufferPointer(start: $0, count: capacity))
-        }
-    }
-}
-
-extension UInt16 {
-    var bytes: [UInt8] {
-        return _convertToBytes(self, withCapacity: MemoryLayout<Self>.size)
-    }
-    
-    init?(_ bytes: [UInt8]) {
-        guard bytes.count == MemoryLayout<Self>.size else { return nil }
-        self = bytes.withUnsafeBytes {
-            return $0.load(as: Self.self)
-        }
-    }
-}
-
-extension DWord {
-    var bytes: [UInt8] {
-        return _convertToBytes(self, withCapacity: MemoryLayout<Self>.size)
     }
 }
 
@@ -134,7 +83,7 @@ public struct VersionTag {
 public struct Limits {
     static let infoTextMaxLength = 256 - 1 // max length of info chunk text minus zero terminator
     static let terminatorSampleLength = 46 // the length of terminator samples, in sample data points
-    static let sampleNamMaxLength = 20 - 1 // max length of sample name (excluding the terminator byte)
+    static let sampleNameMaxLength = 20 - 1 // max length of sample name (excluding the terminator byte)
 }
 
 extension Character {
--- Tests/SaffronTests/SaffronTests.swift
+++ Tests/SaffronTests/SaffronTests.swift
@@ -2,5 +2,32 @@ import XCTest
 @testable import Saffron
 
 final class SaffronTests: XCTestCase {
-
+    override func setUp() {
+        super.setUp()
+        
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        
+    }
+    
+    func testFourCCFromString() {
+        let riffString = "RIFF"
+        let fourCC = riffString.toFourCC()
+        XCTAssertEqual(fourCC, 0x52494646)
+    }
+    
+    func testByteArrayFromFourCC() {
+        let fourCC: FourCC = 0x46464952
+        let ba = fourCC.bytes
+        XCTAssertEqual(ba, [0x52, 0x49, 0x46, 0x46])
+    }
+    
+    func testByteArrayFromDWordLE() {
+        let size: DWord = 0x01DD501A  // 31_281_178, in file as LE: 1A 50 DD 01
+        let ba = size.bytes
+        XCTAssertEqual(ba, [0x1A, 0x50, 0xDD, 0x01])
+    }
+    
 }
